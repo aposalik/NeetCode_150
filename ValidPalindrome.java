7fa8684f14054d90a4723f0afe296aa4796d57f6
Abdullah_Salik <118934390+aposalik@users.noreply.github.com>
2026-03-28 23:50:51 +0200

    Update ValidPalindrome.java

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        String word = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int len = word.length();
+        boolean result = false;
+
+        for(int i=0; i<len/2; i++){
+            if(word.charAt(i) != word.charAt(len-1-i)){
+                return false;
+            }
+            result = true;
+        }
+        return result;
     }
 }
